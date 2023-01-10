73d8ad6db2938673cd8dbd4efb1dc831a84c7502
czesster <102236299+czesster@users.noreply.github.com>
2023-01-10 15:01:30 +0100

    cleaned up code, moved to class model

--- script.js
+++ script.js
@@ -1,35 +1,45 @@
 "use strict";
+class WeatherApp {
+  // https://openweathermap.org/api/one-call-3
+  apiKey = "3de7bd79ff351d443868c0e738e3bc08";
 
-// https://openweathermap.org/api/one-call-3
-const apiKey = "3de7bd79ff351d443868c0e738e3bc08";
-let city = "Łódź";
+  constructor() {}
 
-// API url to be fetched
-const url = (city, apiKey) =>
-  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`; // works by city
+  // API url to be fetched
+  getUrl(city, apiKey) {
+    return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  }
 
-// fetch data from api using given city
-async function getWeatherByCity(city, apiKey) {
-  const resp = await fetch(url(city, apiKey));
-  const respData = await resp.json();
-  console.log(respData);
+  // fetch data from api using given city
+  async getWeatherByCity(city, apiKey) {
+    const resp = await fetch(this.getUrl(city, apiKey));
+    const respJSON = await resp.json();
+    console.log(respJSON);
+    // return respJSON;
+  }
 }
 
+const weatherApp = new WeatherApp();
+
+// ///////////////////////////////////////////////////////////////
+// /////////////////// EVENT LISTENERS ///////////////////////////
+// ///////////////////////////////////////////////////////////////
 const input = document.querySelector("[data-form-input]");
 const formBtn = document.querySelector("[data-form-button]");
-const year = document.querySelector("[data-year]");
 
 let clicked = false;
 
 formBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  const inputText = input.value;
+  const inputText = input.value.toLowerCase();
   console.log(inputText);
-  getWeatherByCity(inputText, apiKey);
+  weatherApp.getWeatherByCity(inputText, weatherApp.apiKey);
 
   clicked = true;
 });
 
+// Update year in footer
+const year = document.querySelector("[data-year]");
 const updateYear = () => {
   year.textContent = new Date().getFullYear();
   console.log(new Date().getFullYear());
